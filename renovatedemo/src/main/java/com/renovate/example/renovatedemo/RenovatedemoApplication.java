3a8963303455ae691db91265fa1dda674e18062f
Vernekar <shreyasanand.vernekar@fujitsu.com>
2023-04-18 15:45:15 +0530

    committing the project structure

---
+++ renovatedemo/src/main/java/com/renovate/example/renovatedemo/RenovatedemoApplication.java
@@ -0,0 +1,13 @@
+package com.renovate.example.renovatedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RenovatedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RenovatedemoApplication.class, args);
+	}
+
+}
---
+++ renovatedemo/src/test/java/com/renovate/example/renovatedemo/RenovatedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.renovate.example.renovatedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenovatedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
